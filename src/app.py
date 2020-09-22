4aa8d0c303f53514e5d49f4097dcfabe439a8aaf
Jack Downes <14843574@student.curtin.edu.au>
2020-09-22 10:44:33 +0800

    add test-config argument to app initialisation

--- src/app.py
+++ src/app.py
@@ -1,29 +1,33 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+    @app.route('/')
+    def index():
+        return 'Index Page'
+        
+    @app.route("/1")
+    def test1():
+        return render_template('test.html')
+
+    @app.route("/2")
+    def test2():
+        return render_template('test2.html')
+
+    @app.route("/3")
+    def test3():
+        return render_template('test3.html')
+    return app
     
-@app.route("/1")
-def test1():
-    return render_template('test.html')
-
-@app.route("/2")
-def test2():
-    return render_template('test2.html')
-
-
-@app.route("/3")
-def test3():
-    return render_template('test3.html')
-    
-
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True, host='0.0.0.0')
