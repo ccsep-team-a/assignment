72ac0e5c6ecba5c9636db2aea92f9497048cfd96
xDamon <damonezard@gmail.com>
2020-09-28 16:47:03 +0800

    Update functional tests

--- test/test_app.py
+++ test/test_app.py
@@ -30,19 +30,12 @@ def app_fixture():
 def test_errorhandlers(client):
     """Error handling is not a feature."""
     rv = client.get('/foobar')
-    assert b'404' in rv.data
+    assert b'Page doesn\'t exist' in rv.data
 
 def test_route_index(client):
     """Root should return the page index with expected title."""
     rv = client.get('/')
-
     assert b'SHOPII' in rv.data
-
-def test_route_about(client):
-    """should return data"""
-
-    rv = client.get('/about')
-    assert b'123' in rv.data
 
 def test_route_search(client, app_fixture):
     """should return data"""
